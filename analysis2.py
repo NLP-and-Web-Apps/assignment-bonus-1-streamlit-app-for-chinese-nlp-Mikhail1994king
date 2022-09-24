67e3fc4020ea9116f9e5faff37999e73328c850d
米哈伊尔 <mihayiermihayier@mihayiers-MacBook-Air.local>
2022-09-24 15:22:05 +0800

    djkfjdfk

--- analysis2.py
+++ analysis2.py
@@ -7,14 +7,34 @@ from snownlp import SnowNLP
 st.header("Sentiment Analysis")
 with st.expander('情感分析'):
     text = st.text_input("請輸入文字")
+    s1= SnowNLP(text)
+    st.markdown("sentiment result")
     for sen in text:
-      st.markdown('**Sentiment Result**')
       s = SnowNLP(sen)
       st.write(list(s.tags))
-      st.write(s.sentiments)
+      st.write(s.han)
 
 
-    
+try:
+  st.balloons()
+  st.markdown('**Sentence Sentiment Result**')
+  st.write(s1.sentiments)
+except ValueError as e:
+  print(e)
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
 
 
 
