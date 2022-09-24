c00bfc81a3297e4e38848c51a33b40d895326ed8
米哈伊尔 <mihayiermihayier@mihayiers-MacBook-Air.local>
2022-09-24 13:42:36 +0800

    dfjhhfdj

--- analysis2.py
+++ analysis2.py
@@ -5,11 +5,15 @@ from snownlp import SnowNLP
 
 
 st.header("Sentiment Analysis")
-with st.expander('Analyze text'):
-    text = st.text_input("Input screen name")
+with st.expander('情感分析'):
+    text = st.text_input("請輸入文字")
     for sen in text:
+      st.markdown('**Sentiment Result**')
       s = SnowNLP(sen)
+      st.write(list(s.tags))
       st.write(s.sentiments)
+
+
     
 
 
